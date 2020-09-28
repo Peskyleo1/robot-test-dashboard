178f7a77cdfc944d9e1eff6da5d85a8c85746ce0
Leonardo <peskyleo1@gmail.com>
2020-09-28 17:23:44 +0200

    🏗 - New Build

---
+++ build/precache-manifest.7f8b2338eb121015b5fafe3bd8bef0ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2522cf66638ef311e25e1f7fcdd442",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b615c097250ed781e015",
+    "url": "./static/css/main.a29a3155.chunk.css"
+  },
+  {
+    "revision": "03abe782a958ca719d59",
+    "url": "./static/js/2.655d934e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.655d934e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b615c097250ed781e015",
+    "url": "./static/js/main.0e04cd5e.chunk.js"
+  },
+  {
+    "revision": "da635c583b4f22a7f232",
+    "url": "./static/js/runtime-main.9a509c85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca6d8373700689aee27e85f42c83a611",
+    "url": "./static/media/map.ca6d8373.PNG"
+  },
+  {
+    "revision": "b3375870b89e57698ad2d946b3570665",
+    "url": "./static/media/nav.b3375870.png"
+  }
+]);
